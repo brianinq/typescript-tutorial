b16719ff49fc49e8f8f047adc43ea092651b6aed
Brian Kipkoech <73099800+brianinq@users.noreply.github.com>
2023-01-17 10:54:37 +0000

    html to react

--- src/main.ts
+++ src/main.ts
@@ -1,143 +1,140 @@
 let firstName: string = "Dylan";
 console.log(firstName);
 
-const json = JSON.parse("55")
+const json = JSON.parse("55");
 console.log(typeof json);
 
-const names: string[] = []
-names.push("Brian")
-names.push("power")
+const names: string[] = [];
+names.push("Brian");
+names.push("power");
 
 console.log(names);
 
-const titles: readonly string[] = ["Mr", "Miss", "Mrs"]
+const titles: readonly string[] = ["Mr", "Miss", "Mrs"];
 
-const readonlyTuple: readonly [number, string, boolean] = [1, "Two", false]
+const readonlyTuple: readonly [number, string, boolean] = [1, "Two", false];
 console.log(readonlyTuple);
 
-const namedTuple: readonly [x: number, y:number, z:number] = [2,3,4]
+const namedTuple: readonly [x: number, y: number, z: number] = [2, 3, 4];
 console.log(typeof namedTuple);
-const [x, y, z] = namedTuple
+const [x, y, z] = namedTuple;
 
-
-const car: {type: string, model: string, year:number} = {
-    type: "Toyota",
-    model: "Prius",
-    year: 2016,
-}
-const car2: {type: string, millage?: number}= {
-    type: "Toyota"
-}
-car2.millage = 5000
+const car: { type: string; model: string; year: number } = {
+  type: "Toyota",
+  model: "Prius",
+  year: 2016,
+};
+const car2: { type: string; millage?: number } = {
+  type: "Toyota",
+};
+car2.millage = 5000;
 
 // index signatures
 
-const nameAgeMap: {[index: string]: number} ={}
-nameAgeMap.brian = 45
+const nameAgeMap: { [index: string]: number } = {};
+nameAgeMap.brian = 45;
 // nameAgeMap.brian = "forty" //throws an error string not asignable to number
-
 
 //enums
 
 //numeric
 
-enum Cardinaldirections  {
-    North ,
-    East,
-    South,
-    West
+enum Cardinaldirections {
+  North,
+  East,
+  South,
+  West,
 }
 
 enum StatusCodes {
-    NotFound = 404,
-    Success = 200,
-    Accepted =202,
-    BadRequest =400
+  NotFound = 404,
+  Success = 200,
+  Accepted = 202,
+  BadRequest = 400,
 }
 
 //String Enums
 
 enum CardinalDirections {
-  North = 'North',
+  North = "North",
   East = "East",
   South = "South",
-  West = "West"
-};
+  West = "West",
+}
 
 //Type aliases
-type CarYear = number
-type CarType = string
-type CarModel= string
+type CarYear = number;
+type CarType = string;
+type CarModel = string;
 
 type Car = {
-    type: CarType,
-    year: CarYear,
-    model: CarModel
-}
-const carYear: CarYear = 2001
-const carType: CarType = "Toyota"
-const carModel: CarModel = "Corolla"
+  type: CarType;
+  year: CarYear;
+  model: CarModel;
+};
+const carYear: CarYear = 2001;
+const carType: CarType = "Toyota";
+const carModel: CarModel = "Corolla";
 const car3: Car = {
   year: carYear,
   type: carType,
-  model: carModel
+  model: carModel,
 };
 
 // Interfaces (objects only)
 
-
-interface Rectangle_{
-    height: number,
-    width: number
+interface Rectangle_ {
+  height: number;
+  width: number;
 }
 const rectc: Rectangle_ = {
-    height: 78,
-    width: 200
-}
+  height: 78,
+  width: 200,
+};
 
 //extending interfaces
-interface ColoredRectangle extends Rectangle_{
-    color: string
+interface ColoredRectangle extends Rectangle_ {
+  color: string;
 }
 
 const redRect: ColoredRectangle = {
-    height: 67,
-    width: 90,
-    color: "red"
-}
+  height: 67,
+  width: 90,
+  color: "red",
+};
 
 // union types
-type StatusCode = string | number 
+type StatusCode = string | number;
 
-const printStatusCode= (code: StatusCode)=>{
-    console.log(code);
-}
-printStatusCode("Not Found ")
-printStatusCode(404)
+const printStatusCode = (code: StatusCode) => {
+  console.log(code);
+};
+printStatusCode("Not Found ");
+printStatusCode(404);
 
 // functions
 
-function multiply(a: number, b:number, c?:number, d: number = 2): number {
-    return a* b * (c || 1) * d
+function multiply(a: number, b: number, c?: number, d: number = 2): number {
+  return a * b * (c || 1) * d;
 }
 
-function logger(num:number): void {
-    console.log(num);
+function logger(num: number): void {
+  console.log(num);
 }
-logger(multiply(56, 89))
+logger(multiply(56, 89));
 
 //function type aliases
 
-type Negate = (value: number)=> number
-const negate: Negate = (value)=> value * -1
-console.log(negate(90))
+type Negate = (value: number) => number;
+const negate: Negate = (value) => value * -1;
+console.log(negate(90));
 
 //casting
 
-let g: unknown = 'hello';
+let g: unknown = "hello";
 console.log((<string>g).length);
 
-console.log(((8 as unknown) as string).length)
+console.log((8 as unknown as string).length);
 
 //classes inheritance
 
@@ -153,7 +150,6 @@ class Rectangle implements Shape {
   }
 }
 
-
 // Partial, Record, Required, Omit, Pick, Exclude, ReturnType, Parameters
 
 interface Person {
@@ -162,13 +158,13 @@ interface Person {
   location?: string;
 }
 
-const bob: Omit<Person, 'age' | 'location'> = {
-  name: 'Bob'
+const bob: Omit<Person, "age" | "location"> = {
+  name: "Bob",
   // `Omit` has removed age and location from the type and they can't be defined here
 };
 
-const boby: Pick<Person, 'name'> = {
-  name: 'Bob'
+const boby: Pick<Person, "name"> = {
+  name: "Bob",
   // `Pick` has only kept name, so age and location were removed from the type and they can't be defined here
 };
 
@@ -178,5 +174,5 @@ interface Person {
   location?: string;
 }
 
-type Primitive = string | number | boolean
+type Primitive = string | number | boolean;
 const value: Exclude<Primitive, string> = true; // a string cannot be used here since Exclude removed it from the type.
